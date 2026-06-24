be40b3d8424b0b69024cd2d54fe6ae1d6b1db7d5
shubhamhotchandani07-sudo <shubhamhotchandani07@gmail.com>
2026-06-23 23:56:35 -0700

    ifelifelse.python

---
+++ ifelifelse.py
@@ -0,0 +1,72 @@
+#AGE CATEGORY CHECKER
+
+#age=int(input("Enter a age"))
+#if(age<=10):
+# print("Child")
+#elif(age<=18):
+# print("Teenager")
+#elif(age<=35):
+# print("Adult")
+#else: 
+ #print("Senior")
+
+#GRADE CALCULATOR
+
+#grade=int(input("Enter a grade"))
+#if(grade>=90):
+ #print("A")
+#elif(grade>=80):
+# print("B")
+#elif(grade>=70):
+ #print("C")
+#elif(grade>=60):
+ #print("D")
+#elif(grade>=50):
+ #print("E")
+#elif(grade>=40):
+ #print("F")
+#else:
+# print("Fail")
+
+#VOWEL OR CONSONENT
+
+#char=(input("Enter a character"))
+#if(char=="a"):
+ #print("Vowel")
+#elif(char=="e"):
+ #print("Vowel")
+#elif(char=="i"):
+ #print("Vowel")
+#elif(char=="o"):
+ #print("Vowel")
+#elif(char=="u"):
+ #print("Vowel")
+#else:
+ #print("not vowel")
+
+#NUMBER COMPARISON
+
+#number1=int(input("enter a number"))
+#number2=int(input("enter a number"))
+#number3=int(input("enter a number"))
+#if(number1>number2): 
+ #print("number1 is largest")
+#elif(number2>number3):
+ #print("number2 is largest")
+#elif(number1>number3):
+ #print("number1 is greatest")
+#else:
+ #print("smallest")
+
+#NUMBER DIVIDED BY 3 AND 5
+
+number=int(input("Enter a number"))
+if(number%5==0 and number%3==0):
+ print("divide by 3 and 5 both")
+elif(number%3==0):
+ print("Divide by 3")
+elif(number%5==0):
+ print("Divisible by 5")
+else:
+ print("incorrect input")
+
