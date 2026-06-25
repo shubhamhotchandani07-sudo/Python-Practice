50a8f55167b1f5cf9b6ef143df72851288f3770d
shubhamhotchandani07-sudo <shubhamhotchandani07@gmail.com>
2026-06-24 23:50:20 -0700

    While Loop Python

---
+++ whileloop.py
@@ -0,0 +1,41 @@
+#COUNTING FROM 1 TO 50 BY WHILE LOOP
+
+count=1
+while count<=50:
+    print(count)
+    count=count+1
+
+#COUNTING FROM 50 TO 1 BY WHILE LOOP
+
+count=50
+while count>=1:
+    print(count)
+    count=count-1
+
+#COUNTING FROM -1 TO -50 BY WHILE LOOP
+
+count=-1
+while count>=-50:
+    print(count)
+    count=count-1
+
+#COUNTING FROM -50 TO -1 BY WHILE LOOP
+
+count=-50
+while count<=-1:
+    print(count)
+    count=count+1
+
+#PRINT ODD NUMBERS FROM 10-20
+
+numbers=11
+while numbers<=20:
+  print(numbers)
+  numbers=numbers+2
+
+#PRINT EVEN NUMBERS FROM 50 TO 80
+
+numbers=50
+while numbers<=80:
+    print(numbers)
+    numbers=numbers+2 
