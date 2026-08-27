2a1a40fc6dc9a3ed68616328738cca1c3a6b1593
S <shubhamhotchandani07@gmail.com>
2026-08-26 22:12:19 -0700

    Import Module in Python

---
+++ Modules_Import.py
@@ -0,0 +1,24 @@
+#1st METHOD OF IMPORTING BY FROM KEYWORD IN THIS ONLY THOSE METHOD COMES WHCH WE WRITE AFTER IMPORT
+from Classes import dog , user
+
+dog.boss
+
+#2nd METHOD OF IMPORTING DIRECTLY BY CLASS NAME
+import Class_Inheritance
+
+Class_Inheritance.patient
+
+#3rd METGOD OF IMPORTING BY FROM KEYWORD
+from Functions import vowels
+vowels("Rohit")
+
+from Functions import *
+even()
+
+#4th METHOD OF IMPORTING  BY CHANGING METHOD NAME BY "AS" KEYWORD
+from Functions import  even_odd_checker as even
+
+#IMPORTING FROM THE OTHER FOLDER 
+from folder1.cal import hello
+hello(10,5)
+
